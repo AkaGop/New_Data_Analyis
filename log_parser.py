e9f282f6869a06130a04fc7b4b27017482c8d51c
AkaGop <150795195+AkaGop@users.noreply.github.com>
2025-10-13 19:50:40 +0200

    Create log_parser.py

---
+++ log_parser.py
@@ -0,0 +1,7 @@
+# log_parser.py
+"""
+This module will contain all log parsing logic.
+"""
+def parse_log_file(uploaded_file):
+    # This is a placeholder. It will be replaced in the next step.
+    return [{"message": "Parser is connected!"}]
